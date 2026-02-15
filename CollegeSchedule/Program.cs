4b7da2cc57df8ff6f41646ee269dcde6ff46934a
VladGrevtsov07 <russiarideronlan@gmail.com>
2026-02-15 20:17:19 +0700

    Созданы DTO для формирования ответа API

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -1,6 +1,21 @@
+using CollegeSchedule.Data;
+using CollegeSchedule.Middlewares;
+using CollegeSchedule.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+DotNetEnv.Env.Load();
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+ $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+ $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+ $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+
+$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+builder.Services.AddDbContext<AppDbContext>(options =>
+ options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +34,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
--- CollegeSchedule/obj/Debug/net8.0/CollegeSchedule.AssemblyInfo.cs
+++ CollegeSchedule/obj/Debug/net8.0/CollegeSchedule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2dbcdb2f27ba6e979951ccff7dfa3c68ad3d12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a732d292059a12d9d9811b3b4ade92850990d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
