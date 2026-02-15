1697add65d4f3e5f3e88073781b3fc4bec3e9e96
VladGrevtsov07 <russiarideronlan@gmail.com>
2026-02-15 19:44:33 +0700

    Добавлены пакеты для работы с PostgreSQL и EF Core

--- CollegeSchedule/obj/Debug/net8.0/CollegeSchedule.AssemblyInfo.cs
+++ CollegeSchedule/obj/Debug/net8.0/CollegeSchedule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ef89370d3c41ef490813341e2f77dcb42cc208")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
